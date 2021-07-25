7fe79b2d2c3235de9722ff2a34c2619bd7039401
phys-ken <cromartie24@icloud.com>
2021-07-26 00:26:35 +0900

    fix title

---
+++ docx_pptx2md.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+find inputf -type d | xargs -I dir mkdir outputf/dir
+
+find ./ -iname "*.docx" -type f | while read FILE
+do
+    path=${FILE#./}
+    string_filename=${path##*/}
+    string_filename_without_extension=${string_filename%.*}
+    string_path=${path%/*}
+    md_path=${string_path#input}
+    md_file_name=${string_filename_without_extension}.md
+    md_output_path=${md_path}/${md_file_name}
+
+    echo "##File Info##"
+    echo $path
+    echo ${string_filename}
+    echo ${string_filename_without_extension}
+    echo ${string_path}
+    echo $md_path
+    echo $md_file_name
+    echo "output path"
+    echo $md_output_path
+    echo "#############"
+    mkdir ./outputf/${md_path}/${string_filename_without_extension}/
+    pandoc .${path}  --extract-media ./outputf/${md_path}/${string_filename_without_extension}/  -o ./outputf/${md_output_path} 
+    
+    python replace_path.py ./outputf/${md_output_path} ./outputf/${md_path}/
+
+done
+
+python pptx2md.py
